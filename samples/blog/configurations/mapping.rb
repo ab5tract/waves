a16cabeb0fb8ef7011b92ea61c258d33ac04a0b4
Dan Yoder <dan@zeraweb.com>
2008-06-03 19:15:14 -0700

    Got rid of pseudo-english and / or syntax.

--- samples/blog/configurations/mapping.rb
+++ samples/blog/configurations/mapping.rb
@@ -5,7 +5,7 @@ module Blog
     module Mapping
       extend Waves::Mapping
       path '/comments', :method => :post do
-        with(:comments).create.and.redirect( :model => :entry, :name => instance.name )
+        with(:comments).create and redirect( :model => :entry, :name => instance.name )
       end
       include Waves::Mapping::PrettyUrls::RestRules
       include Waves::Mapping::PrettyUrls::GetRules
